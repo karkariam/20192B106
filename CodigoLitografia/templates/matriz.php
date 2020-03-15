f155afa822d6cb7b401fe338a196071d34a7e4f3
ireik <ediyahir22@hotmail.com>
2020-03-15 18:27:05 -0500

    solución de validación en la matriz

--- CodigoLitografia/templates/matriz.php
+++ CodigoLitografia/templates/matriz.php
@@ -15,26 +15,26 @@
     <form action="matriz_sol.php" method="post" enctype="multipart/form-data">
 
         <fieldset id="matrix1">
-            <table align="center">
+            <table align="center" >
                 <tr>
 
-                    <td><input type="text" name="1-0-0" requiered /></td>
-                    <td><input type="text" name="1-0-1" requiered /></td>
-                    <td><input type="text" name="1-0-2" requiered /></td>
+                    <td><input type="text" name="1-0-0" required /></td>
+                    <td><input type="text" name="1-0-1" required /></td>
+                    <td><input type="text" name="1-0-2" required /></td>
 
                 </tr>
                 <tr>
 
-                    <td><input type="text" name="1-1-0" requiered /></td>
-                    <td><input type="text" name="1-1-1" requiered /></td>
-                    <td><input type="text" name="1-1-2" requiered /></td>
+                    <td><input type="text" name="1-1-0" required /></td>
+                    <td><input type="text" name="1-1-1" required /></td>
+                    <td><input type="text" name="1-1-2" required /></td>
 
                 </tr>
                 <tr>
 
-                    <td><input type="text" name="1-2-0" requiered /></td>
-                    <td><input type="text" name="1-2-1" requiered /></td>
-                    <td><input type="text" name="1-2-2" requiered /></td>
+                    <td><input type="text" name="1-2-0" required /></td>
+                    <td><input type="text" name="1-2-1" required /></td>
+                    <td><input type="text" name="1-2-2" required /></td>
 
                 </tr>
 
@@ -46,33 +46,33 @@
         </p>
 
         <fieldset id="matrix2">
-            <table align="center">
+            <table align="center" required>
                 <tr>
 
-                    <td><input type="text" name="2-0-0" requiered /></td>
-                    <td><input type="text" name="2-0-1" requiered /></td>
-                    <td><input type="text" name="2-0-2" requiered /></td>
+                    <td><input type="text" name="2-0-0" required /></td>
+                    <td><input type="text" name="2-0-1" required /></td>
+                    <td><input type="text" name="2-0-2" required /></td>
 
                 </tr>
                 <tr>
 
-                    <td><input type="text" name="2-1-0" requiered /></td>
-                    <td><input type="text" name="2-1-1" requiered /></td>
-                    <td><input type="text" name="2-1-2" requiered /></td>
+                    <td><input type="text" name="2-1-0" required /></td>
+                    <td><input type="text" name="2-1-1" required /></td>
+                    <td><input type="text" name="2-1-2" required /></td>
 
                 </tr>
                 <tr>
 
-                    <td><input type="text" name="2-2-0" requiered /></td>
-                    <td><input type="text" name="2-2-1" requiered /></td>
-                    <td><input type="text" name="2-2-2" requiered /></td>
+                    <td><input type="text" name="2-2-0" required /></td>
+                    <td><input type="text" name="2-2-1" required /></td>
+                    <td><input type="text" name="2-2-2" required /></td>
 
                 </tr>
 
             </table>
         </fieldset>
 
-        <input type="submit" name="Enviar" value="Operar" />
+        <input type="submit" name="Enviar" value="Operar"/>
     </form>
 
 </div>
